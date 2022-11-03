e290553f21a2dbe5292c4b7719360fdfc723f280
jfrancisc <franciis.cevallos@gmail.com>
2022-11-03 17:10:48 -0500

    subo

--- data/egresos/buscar_codigo.php
+++ data/egresos/buscar_codigo.php
@@ -14,26 +14,27 @@ $consultapuntoresult = pg_query("select id_punto_venta from punto_venta_empresa 
 while ($row = pg_fetch_row($consultapuntoresult)) {
     $conpuntoresult = $row[0];
 }
-$consulta = pg_query("select * from productos where codigo like '%$texto2%' and estado = 'Activo'");
+$consulta = pg_query("select cod_productos,articulo,cod_barras,codigo,precio_compra,iva_minorista,iva,cod_productos,incluye_iva,venta_promedio from productos where codigo like '%$texto2%' and estado = 'Activo'");
 if (pg_num_rows($consulta) > 0) {
-    while ($row = pg_fetch_row($consulta)) {
-        $consulta1 = pg_query("select * from   productos p left join detalle_producto_bodega dpb on p.cod_productos=dpb.cod_productos where p.cod_productos=$row[0] and dpb.id_bodega=$conpuntoresult ");
-        $row1 = pg_fetch_row($consulta1);
-           if($row[37]==""){
-            $row[37]=$row[6];
+    while ($row = pg_fetch_assoc($consulta)) {
+        $consulta1 = pg_query("select dpb.stock from   productos p left join detalle_producto_bodega dpb on p.cod_productos=dpb.cod_productos where p.cod_productos='$row[cod_productos]' and dpb.id_bodega=$conpuntoresult ");
+        $row1 = pg_fetch_assoc($consulta1);
+           if($row['venta_promedio']==""){
+           $row['venta_promedio']=$row['precio_compra'];
         }else{
-           $row[37]=$row[37]; 
+          $row['venta_promedio']=$row['venta_promedio']; 
         }
-        $data[] = array(
-            'value' => $row[1],
-            'codigo_barras' => $row[2],
-            'producto' => $row[3],
-            'precio' => $row[37],
-            'p_venta' => $row[9],
-            'iva_producto' => $row[4],
-            'cod_producto' => $row[0],
-            'incluye' => $row[26],
-            'disponibles' => $row1[46]
+      
+           $data[] = array(
+            'value' => $row['codigo'],
+            'codigo_barras' => $row['cod_barras'],
+            'producto' => $row['articulo'],
+            'precio' => $row['venta_promedio'],
+            'p_venta' => $row['iva_minorista'],
+            'iva_producto' => $row['iva'],
+            'cod_producto' => $row['cod_productos'],
+            'incluye' => $row['incluye_iva'],
+            'disponibles' => $row1['stock']
         );
     }
     echo $data = json_encode($data);
--- data/egresos/search.php
+++ data/egresos/search.php
@@ -21,26 +21,26 @@ while ($row = pg_fetch_row($consultapuntoresult)) {
 }
 
 if ($codigo_barras != "") {
-    $consulta = pg_query("select * from productos where (cod_barras = '$codigo_barras'  or codigo='$codigo') and estado = 'Activo'");
+    $consulta = pg_query("select cod_productos,articulo,cod_barras,codigo,precio_compra,iva_minorista,iva,cod_productos,incluye_iva,venta_promedio from productos where (cod_barras = '$codigo_barras'  or codigo='$codigo') and estado = 'Activo'");
 
-    $consulta1 = pg_query("select * from productos p left join detalle_producto_bodega dpb on p.cod_productos=dpb.cod_productos "
+    $consulta1 = pg_query("select dpb.cod_productos,articulo,cod_barras,codigo,precio_compra,iva_minorista,iva,incluye_iva,venta_promedio,dpb.stock  from productos p left join detalle_producto_bodega dpb on p.cod_productos=dpb.cod_productos "
             . "where (p.cod_barras='$codigo_barras' or codigo='$codigo') and dpb.id_bodega=$conpuntoresult ");
 //    $row1 = pg_fetch_row($consulta1);
     
-    while ($row = pg_fetch_row($consulta1)) {
-           if($row[37]==""){
-            $row[37]=$row[6];
+    while ($row = pg_fetch_assoc($consulta1)) {
+         if($row['venta_promedio']==""){
+           $row['venta_promedio']=$row['precio_compra'];
         }else{
-           $row[37]=$row[37]; 
+          $row['venta_promedio']=$row['venta_promedio']; 
         }
-        $arr_data[] = strtoupper($row[1]);
-        $arr_data[] = $row[3];
-        $arr_data[] = $row[37];
-        $arr_data[] = $row[9];
-        $arr_data[] = $row[4];
-        $arr_data[] = $row[0];
-        $arr_data[] = $row[26];
-        $arr_data[] = $row[46];
+        $arr_data[] = strtoupper($row['cod_barras']);
+        $arr_data[] = $row['articulo'];
+        $arr_data[] = $row['venta_promedio'];
+        $arr_data[] = $row['iva_minorista'];
+        $arr_data[] = $row['iva'];
+        $arr_data[] = $row['cod_productos'];
+        $arr_data[] = $row['incluye_iva'];
+        $arr_data[] = $row['stock'];
     }
 }
 echo json_encode($arr_data);
