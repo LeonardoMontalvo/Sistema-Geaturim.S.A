09b6cbe715da52a5777a8d048bd1f6b01902e7a2
Javier Paucar <javierpaucar1200@gmail.com>
2023-01-23 09:09:26 -0500

    cambiado consultas en cargar_tipo_pago.php

--- data/cuentas_cobrar/cargar_tipo_pago.php
+++ data/cuentas_cobrar/cargar_tipo_pago.php
@@ -6,7 +6,7 @@ conectarse();
 error_reporting(0);
 
 //////////////////consulta 1/////////////////
-$consulta = pg_query("select * from factura_venta F, 
+$consulta = pg_query("select F.id_factura_venta from factura_venta F, 
                     pagos_venta P,formas_pago_mixto fpm 
                     where  f.id_factura_venta=fpm.id_factura_venta 
                     and fpm.forma_pago='CREDITO' 
@@ -15,9 +15,9 @@ $consulta = pg_query("select * from factura_venta F,
                     and F.id_factura_venta = P.id_factura_venta 
                     and fpm.estado='Activo' 
                     and P.estado='Activo' 
-                    and P.id_empresa='$_SESSION[PV]'");
+                    and P.id_empresa='$_SESSION[PV]' limit 1");
 
-$consultanv=pg_query("select * from facturas_novalidas F, 
+$consultanv=pg_query("select F.id_facturas_novalidas from facturas_novalidas F, 
                     pagos_venta P,formas_pago_mixto fpm 
                     where  f.id_facturas_novalidas=fpm.id_factura_venta 
                     and fpm.forma_pago='CREDITO' 
@@ -26,7 +26,7 @@ $consultanv=pg_query("select * from facturas_novalidas F,
                     and F.id_facturas_novalidas = P.id_factura_venta 
                     and fpm.estado='Activo' 
                     and P.estado='Activo' 
-                    and P.id_empresa='$_SESSION[PV]'");               
+                    and P.id_empresa='$_SESSION[PV]' limit 1");               
                     
 if (pg_num_rows($consulta) > 0||pg_num_rows($consultanv) > 0) {
     echo "<option id=INTERNA value=INTERNA >INTERNA</option>";
