ad6accffcbc8bee8ffb40fbfb95e592a2be95319
Javier Paucar <javierpaucar1200@gmail.com>
2023-06-07 15:15:17 -0500

    limitar resultados de busqueda de clientes en factura de venta a 200 por busqueda

--- data/factura_venta/buscar_cliente.php
+++ data/factura_venta/buscar_cliente.php
@@ -7,7 +7,7 @@ $texto = $_GET['term'];
 $consulta = pg_query("select identificacion,id_cliente,nombres_cli,direccion_cli,telefono ,correo,nombre_vendedor,vendedores.id_vendedor
 from clientes inner join rutas on rutas.id_ruta=clientes.credito_cupo
 left join vendedores on vendedores.id_vendedor=rutas.id_vendedor
-where clientes.estado='Activo' and rutas.estado='Activo' and identificacion like '%$texto%'");
+where clientes.estado='Activo' and rutas.estado='Activo' and identificacion like '%$texto%' limit 200");
 while ($row = pg_fetch_row($consulta)) {
     $data[] = array(
         'value' => $row[0],
