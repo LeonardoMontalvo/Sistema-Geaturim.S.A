7396d68446a93a2b9d3e2fb64ff8f40b2904fc5f
Javier Paucar <javierpaucar1200@gmail.com>
2023-06-20 15:16:39 -0500

    cambios superf

--- data/kardex_producto/search3.php
+++ data/kardex_producto/search3.php
@@ -9,7 +9,7 @@ $codigo_barras=strtoupper($_GET["codigo_barras"]);
 $arr_data = array();
 
 if ($codigo_barras != "") {
-    $consulta = pg_query("select * from productos where cod_barras = '$codigo_barras' and estado = 'Activo'");
+    $consulta = pg_query("select * from productos where (cod_barras = '$codigo_barras' or codigo='$codigo_barras') and estado = 'Activo'");
     while ($row = pg_fetch_row($consulta)) {
         $arr_data[] = $row[1];
         $arr_data[] = $row[3];
