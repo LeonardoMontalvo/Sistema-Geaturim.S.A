34046971a86393eba293b5e9621126f434e5d667
Javier Paucar <javierpaucar1200@gmail.com>
2023-06-23 09:17:06 -0500

    no mostrar productos sin stock restaurantes

--- data/restaurantes_ordenes/buscar_productos.php
+++ data/restaurantes_ordenes/buscar_productos.php
@@ -33,7 +33,7 @@ from productos p
 left join detalle_producto_bodega dpb
 using(cod_productos) 
 where (cod_barras = '$texto2' or articulo ilike '%$texto2%') and estado = 'Activo'
-and dpb.id_bodega=$puntoventa 
+and dpb.id_bodega=$puntoventa and dpb.stock>0
 ";
 
 if (!empty($categoria)) {
