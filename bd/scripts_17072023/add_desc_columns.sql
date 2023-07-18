1cb5915c92150e969fb0aa72d4573c66ab76e585
Javier Paucar <javierpaucar1200@gmail.com>
2023-07-18 11:04:03 -0500

    aplicando cambios descuento hashem

---
+++ bd/scripts_17072023/add_desc_columns.sql
@@ -0,0 +1,8 @@
+﻿set search_path to public;
+alter table factura_venta
+add column desc_prod numeric,
+add column desc_fact numeric;
+
+alter table facturas_novalidas
+add column desc_prod numeric,
+add column desc_fact numeric;
