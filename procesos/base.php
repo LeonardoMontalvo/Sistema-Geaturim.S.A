f11827f56aace483961708efe32cdbd43482694f
jfrancisc <franciis.cevallos@gmail.com>
2023-09-15 17:15:41 -0500

    parametrizado reten base

---
+++ procesos/base.php
@@ -0,0 +1,36 @@
+<?php
+
+function conectarse()
+{
+
+    if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=pyssystems user=postgres password=root"))) {
+
+        exit();
+    } else {
+        if (!empty(obtenerCookie("esquema"))) {
+            pg_query("SET search_path TO '" . obtenerCookie("esquema") . "';");
+        }
+    }
+    return $conexion;
+}
+//function conectarse_p()
+//{
+//    if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=sysweb_hashem user=postgres password=root"))) {
+//        exit();
+//    } else {
+//        if (!empty(obtenerCookie("esquema"))) {
+//            pg_query("SET search_path TO '" . obtenerCookie("esquema") . "';");
+//        }
+//    }
+//    return $conexion;
+//}
+
+function obtenerCookie($cookie_name)
+{
+    if (empty($_COOKIE[$cookie_name])) {
+        return null;
+    }
+    return $_COOKIE[$cookie_name];
+}
+
+conectarse();
