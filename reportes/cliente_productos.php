d105f08bfecbadc09bc20c6dd982ad6471587313
jfrancisc <franciis.cevallos@gmail.com>
2023-11-28 09:43:42 -0500

    reporte3

--- reportes/cliente_productos.php
+++ reportes/cliente_productos.php
@@ -33,12 +33,6 @@ class PDF extends FPDF {
         $this->Cell(210, 8, utf8_decode($_SESSION['nombre_empresa']), 0, 1, 'C', 0);
         $this->Image('../images/' . $_SESSION["parametros_empresa"]["logo_empresa"], 10, 7, 15, 15);
         $this->Image('../images/' . $_SESSION["parametros_empresa"]["logo_empresa"], 180, 7, 15, 15);
-        // $this->Cell(180, 5, "PROPIETARIO: " . utf8_decode($_SESSION['propietario']), 0, 1, 'C', 0);
-        // $this->Cell(80, 5, "TEL.: " . utf8_decode($_SESSION['telefono']), 0, 0, 'R', 0);
-        // $this->Cell(80, 5, "CEL.: " . utf8_decode($_SESSION['celular']), 0, 1, 'C', 0);
-        // $this->Cell(180, 5, "DIR.: " . utf8_decode($_SESSION['direccion']), 0, 1, 'C', 0);
-        // $this->Cell(180, 5, "SLOGAN.: " . utf8_decode($_SESSION['slogan']), 0, 1, 'C', 0);
-        // $this->Cell(180, 5, utf8_decode($_SESSION['pais_ciudad']), 0, 1, 'C', 0);
         $this->SetDrawColor(0, 0, 0);
         $this->SetLineWidth(0.4);
         $this->Line(0, 25, 210, 25);
@@ -110,14 +104,8 @@ if ($_GET['idProd'] != "") {
     $id_producto_consult = "";
 }
 if ($_GET['idclientes'] != "") {
-
-
     $consulta = pg_query("select id_cliente,identificacion,nombres_cli from clientes $id_cliente ");
     while ($row = pg_fetch_row($consulta)) {
-
-
-
-
 
         $sql1 = pg_query("select num_factura, factura_venta.fecha_actual, id_factura_venta,usuario.usuario, identificacion,nombres_cli,hora_actual,'VENTAS'
   from factura_venta,usuario ,clientes 
@@ -138,13 +126,14 @@ select comprobante, facturas_novalidas.fecha_actual, id_facturas_novalidas,usuar
         if (pg_num_rows($sql1)) {
             while ($row1 = pg_fetch_row($sql1)) {
 
-                $sql2 = pg_query("
-               select p.codigo, p.articulo, fv.precio_venta, fv.cantidad from detalle_factura_venta fv,productos p where fv.cod_productos=p.cod_productos and id_factura_venta='$row1[2]' $id_producto_consult      
+ $sql2 = pg_query(" select p.codigo, p.articulo, fv.precio_venta, fv.cantidad 
+                    from detalle_factura_venta fv,productos p where fv.cod_productos=p.cod_productos
+                    and id_factura_venta='$row1[2]'  and 'VENTAS'='$row1[7]' $id_producto_consult      
 union
 select p.codigo, p.articulo, fv.precio_venta, fv.cantidad 
 from detalle_facturas_novalidas fv,productos p 
 where fv.cod_productos=p.cod_productos 
-and id_facturas_novalidas='$row1[2]' and fv.estado='Activo'
+and id_facturas_novalidas='$row1[2]' and fv.estado='Activo' and 'NV'='$row1[7]'
 $id_producto_consult ");
 
 
@@ -158,10 +147,10 @@ $id_producto_consult ");
                     $pdf->Cell(35, 6, utf8_decode($row[2]), 1, 0, 'L', true);
                     $pdf->Cell(30, 6, utf8_decode($row[1]), 1, 0, 'L', true);
 
-                    $pdf->Cell(45, 6, utf8_decode("F. NRO.: " . $row1[0]), 1, 0, 'L', true);
-                    $pdf->Cell(35, 6, utf8_decode("FECHA: " . $row1[1]), 1, 0, 'L', true);
-                    $pdf->Cell(35, 6, utf8_decode("HORA: " . $row1[6]), 1, 0, 'L', true);
-                    $pdf->Cell(14, 6, utf8_decode($row1[3]), 1, 0, 'L', true);
+                      $pdf->Cell(33, 6, utf8_decode("F. NRO.: " . $row1[0]), 1, 0, 'L', true);
+                $pdf->Cell(33, 6, utf8_decode("FECHA: " . $row1[1]), 1, 0, 'L', true);
+                $pdf->Cell(33, 6, utf8_decode("HORA: " . $row1[6]), 1, 0, 'L', true);
+                $pdf->Cell(16, 6, utf8_decode($row1[3]), 1, 0, 'L', true);
                 $pdf->Cell(15, 6, utf8_decode($row1[7]), 1, 1, 'L', true);
                     $pdf->SetX(1);
                     $pdf->Cell(34, 6, utf8_decode('Cód. Producto'), 1, 0, 'L', 0);
@@ -189,8 +178,9 @@ $id_producto_consult ");
 
     $sql1 = pg_query("
         
-select num_factura, factura_venta.fecha_actual, id_factura_venta,usuario.usuario, identificacion,nombres_cli,hora_actual,'VENTAS'
- from factura_venta,usuario ,clientes 
+select num_factura, factura_venta.fecha_actual, id_factura_venta,usuario.usuario, identificacion,nombres_cli,hora_actual,'VENTAS' 
+ 
+from factura_venta,usuario ,clientes 
  where factura_venta.fecha_actual $query_fecha '$_GET[fin]'
   and factura_venta.estado='Activo' 
   and factura_venta.id_usuario=usuario.id_usuario
@@ -221,14 +211,14 @@ select comprobante, facturas_novalidas.fecha_actual, id_facturas_novalidas,usuar
      select p.codigo, p.articulo, fv.precio_venta, fv.cantidad
      from detalle_factura_venta fv,productos p 
      where fv.cod_productos=p.cod_productos 
-     and id_factura_venta='$row1[2]'  
+     and id_factura_venta='$row1[2]'  and 'VENTAS'='$row1[7]'
                   $id_producto_consult            
 
 union
 select p.codigo, p.articulo, fv.precio_venta, fv.cantidad 
 from detalle_facturas_novalidas fv,productos p 
 where fv.cod_productos=p.cod_productos 
-and id_facturas_novalidas='$row1[2]' and fv.estado='Activo'
+and id_facturas_novalidas='$row1[2]' and fv.estado='Activo' and 'NV'='$row1[7]'
 $id_producto_consult 
     
 union
@@ -236,7 +226,7 @@ union
 select p.codigo, p.articulo, fv.precio_compra, fv.cantidad 
 from detalle_factura_compra fv,productos p 
 where fv.cod_productos=p.cod_productos 
-and id_factura_compra='$row1[2]'
+and id_factura_compra='$row1[2]' and 'COMPRA'='$row1[7]'
 $id_producto_consult 
                   ");
             
@@ -245,12 +235,12 @@ $id_producto_consult
                 $pdf->SetX(1);
                 $pdf->SetFillColor(216, 216, 231);
                 $pdf->SetFont('helvetica', 'B', 9);
-                $pdf->Cell(35, 6, utf8_decode($row1[5]), 1, 0, 'L', true);
-                $pdf->Cell(30, 6, utf8_decode($row1[4]), 1, 0, 'L', true);
-                $pdf->Cell(45, 6, utf8_decode("F. NRO.: " . $row1[0]), 1, 0, 'L', true);
-                $pdf->Cell(35, 6, utf8_decode("FECHA: " . $row1[1]), 1, 0, 'L', true);
-                $pdf->Cell(35, 6, utf8_decode("HORA: " . $row1[6]), 1, 0, 'L', true);
-                $pdf->Cell(14, 6, utf8_decode($row1[3]), 1, 0, 'L', true);
+                $pdf->Cell(50, 6, utf8_decode($row1[5]), 1, 0, 'L', true);
+                $pdf->Cell(26, 6, utf8_decode($row1[4]), 1, 0, 'L', true);
+                $pdf->Cell(33, 6, utf8_decode("F. NRO.: " . $row1[0]), 1, 0, 'L', true);
+                $pdf->Cell(33, 6, utf8_decode("FECHA: " . $row1[1]), 1, 0, 'L', true);
+                $pdf->Cell(33, 6, utf8_decode("HORA: " . $row1[6]), 1, 0, 'L', true);
+                $pdf->Cell(16, 6, utf8_decode($row1[3]), 1, 0, 'L', true);
                 $pdf->Cell(15, 6, utf8_decode($row1[7]), 1, 1, 'L', true);
                 $pdf->SetX(1);
                 $pdf->Cell(34, 6, utf8_decode('Cód. Producto'), 1, 0, 'L', 0);
