3e868375b3eb75638231da50a24930a8c781ce84
Javier Paucar <javierpaucar1200@gmail.com>
2024-08-26 11:56:11 -0500

    cambios

--- data/cuentas_pagar/index.php
+++ data/cuentas_pagar/index.php
@@ -459,7 +459,7 @@ $cont1++;
     <script src="../../dist/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
     <script src="../../dist/js/jquery.jqGrid.src.js" type="text/javascript"></script>
     <script src="../../dist/js/grid.locale-es.js" type="text/javascript"></script>
-    <script src="cuentasxpagar.js" type="text/javascript"></script>
+    <script src="cuentasxpagar.js?v=1" type="text/javascript"></script>
     <link href="../../dist/css/style.css" rel="stylesheet" type="text/css" />
     <script src="../../dist/js/ventana_reporte.js" type="text/javascript"></script>
 </body>
--- data/proformas/index.php
+++ data/proformas/index.php
@@ -359,7 +359,7 @@ while ($row = pg_fetch_row($consulta7)) {
     <script src="../../dist/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
     <script src="../../dist/js/jquery.jqGrid.src.js" type="text/javascript"></script>
     <script src="../../dist/js/grid.locale-es.js" type="text/javascript"></script>
-    <script src="proforma.js" type="text/javascript"></script>
+    <script src="proforma.js?v=1" type="text/javascript"></script>
     <link href="../../dist/css/style.css" rel="stylesheet" type="text/css" />
     <script src="../../dist/js/ventana_reporte.js" type="text/javascript"></script>
 </body>
