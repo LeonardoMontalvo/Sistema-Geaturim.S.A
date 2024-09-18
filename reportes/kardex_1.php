a4d15a6c788c661c9bfb87874943066b2508f567
Your Name <you@example.com>
2024-09-18 10:57:03 -0500

    karde

--- reportes/kardex_1.php
+++ reportes/kardex_1.php
@@ -120,9 +120,9 @@ if ($_GET['id'] == "") {
 from kardex k
 inner JOIN productos p on k.cod_productos = p.cod_productos
 LEFT JOIN tarifa_impuesto ti on ti.id_taimpuesto = P.id_taimpuesto 
-where  k.fecha_kardex between '$_GET[inicio]' and '$_GET[fin]' "
-            . " and id_empresa=1 Activo "
-            . "order by k.cod_productos,k.id_kardex desc");
+where  k.fecha_kardex between '$_GET[inicio]' and '$_GET[fin]' 
+             and id_empresa=1 and  p.estado='Activo' 
+            order by k.cod_productos,k.id_kardex desc");
     $totalstock = 0;
     $totalpu = 0;
     $totalt = 0;
